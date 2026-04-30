7fc50de24ae0adf490af04a06b3836d84f55f5b9
rushikesh221004 <rushikeshmanjre221004@gmail.com>
2026-04-30 07:22:56 +0000

    bcryptjs package added

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,40 +1,54 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     fullName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     gender: {
-        type: String,
-        enum: ["male", "female", "other"],
-        required: true
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
     },
 
     mobileNo: {
-        type: String,
-        maxLength: 10,
-        unique: true,
-        required: true
+      type: String,
+      maxLength: 10,
+      unique: true,
+      required: true,
     },
 
     email: {
-        type: String,
-        unique: true,
-        required: true
+      type: String,
+      unique: true,
+      required: true,
     },
 
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
 
     refreshToken: {
-        type: String
-    }
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
 
-    
-}, { timestamps: true });
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  next();
+});
+
+userSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = model("User", userSchema);
